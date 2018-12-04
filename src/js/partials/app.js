366bfd6e424a41ab4ab798fe3e749e3749901aee
DenysBondarchuk <bondarchuk520@gmail.com>
2018-12-04 15:16:42 +0200

    start

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,13 @@
 //Some JS
+$('.another__benefit , .another__limitation').click(function(){
+
+	$(this).next('.another__list').toggleClass('dis__block');
+
+	if ($(this).next('.another__list').hasClass('dis__block')) {
+		$(this).find('.another__plus').text('-');
+	} else {
+		$(this).find('.another__plus').text('+');
+	}
+
+	
+});
