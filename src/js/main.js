3356b369d723deea9f4f7948c1d810ab7855fa76
Svetlana Alexeyenko <sv.alexeyenko@gmail.com>
2018-12-06 17:42:35 +0300

    add block use

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 //=require ../../node_modules/slick-carousel/slick/slick.min.js
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
 
+
 //=require partials/app.js
+//=require partials/use.js
+
 
 $(function () {
    objectFitImages()
+});
+
+
+$(document).ready(function () {
+   App.init();
 });
