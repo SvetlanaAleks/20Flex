8f9b3fe3a81577a836d983bd0cb71742a528be8b
Svetlana Alexeyenko <sv.alexeyenko@gmail.com>
2018-12-09 22:37:04 +0300

    merge

--- src/js/partials/use.js
+++ src/js/partials/use.js
@@ -1,7 +1,6 @@
 //Some JS
 const App = (function () {
    "use strict";
-   // const reviewsSlider = $("#js_reviews");
    const DOC = $(document);
    return {
       showComment: function () {
@@ -11,8 +10,29 @@ const App = (function () {
             comment.toggleClass("text-over--full");
          });
       },
+      scrollToTarget: function (scrollSelector, speed) {
+         const links = $(scrollSelector);
+         links.click(function (e) {
+            e.preventDefault();
+            const _this = $(this);
+            const href = _this.attr('href');
+            if (href.length <= 1) {
+               return;
+            }
+            const target = $(href);
+            if (!target.length) {
+               return;
+            }
+            const top = target.offset().top;
+            $('html, body').animate({
+               scrollTop: top
+            }, speed);
+         });
+      },
       init: function () {
          App.showComment();
+         App.scrollToTarget('.js-scroll', 700);
+
       }
    };
 })();
